d332f6fefb7ba07b5271acd272ee9e41f2489df4
User <User@DESKTOP-5LB1FQ5>
2024-12-18 20:32:45 +0300

    Add project files.

---
+++ QuickAssess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickAssess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
